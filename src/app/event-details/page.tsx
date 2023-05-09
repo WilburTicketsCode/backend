bc77bb8784a87c980d9444967b551587c69c8f3d
leticiaribeiro7 <letsribeiro2@gmail.com>
2023-05-08 21:01:08 -0300

    :recycle: refactor: corrigindo responsividade

--- src/app/event-details/page.tsx
+++ src/app/event-details/page.tsx
@@ -5,10 +5,10 @@ import { Card, CardBody, Typography } from '@material-tailwind/react';
 
 export default function Event() {
     return (
-        <div className="mx-auto w-full h-full">
-            <div className='h-1/2 mb-5'>
-                <img className='w-screen h-full overflow-hidden'
-                    src="/img-event-test.jpg"
+        <div className="mx-auto w-full h-full bg-gradient-to-br from-indigo-300 via-purple-800 to-blue-200">
+            <div className='lg:h-[70%] md:h-1/2 sm:h-1/2 mb-5'>
+                <img className='md:w-full md:h-full sm:w-full sm:h-full'
+                    src="/img/event-banner/show_djavan.jpeg"
                     alt='Imagem do evento'
                 />
             </div>
@@ -16,7 +16,7 @@ export default function Event() {
             <Card className='m-5 relative'>
                 <CardBody>
                     <Typography variant="h4" color="blue-gray" className="m-6 text-center">
-                        Show Djavan Turne 2023
+                        Djavan Turne A 2023
                     </Typography>
                 </CardBody>
             </Card>

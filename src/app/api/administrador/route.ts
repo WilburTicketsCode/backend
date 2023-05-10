3b1c0cadccc416bf1b551720a003c16066b50df8
Pedro Henrique A <phaalmeida1@gmail.com>
2023-05-10 13:59:04 -0300

    :sparkles: feat: POST na API do Administrador
    Agora a API do Administrador conta com o POST para inserir dados no BD.
    É necessario receber um json com: nome(String), emai(String), senha(String), cpf(String) e um superAdm(Boolean), onde caso todos os dados sejam validos, o adm será criado, caso não o console ira avisar que deu erro. VERSÃO BETA

--- src/app/api/administrador/route.ts
+++ src/app/api/administrador/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { getAdministradores } from "../../../../lib/administrador";
+import { getAdministradores, inserirAdministrador } from "../../../../lib/administrador";
 
 
 /* Banco pessoal
@@ -14,4 +14,27 @@ export async function GET(request: Request) {
     const data = await getAdministradores()
     
     return NextResponse.json(data)
+}
+
+/* Versão beta do codigo de POST, sofrera mudanças em breve. */
+export async function POST(request:Request) {
+    const dados = await request.json()
+    try {
+        const nome = dados.nome
+        const email = dados.email
+        const senha = dados.senha
+        const cpf = dados.cpf
+        const superAdm = dados.superAdm
+
+        const adm = await inserirAdministrador(nome, email, senha, cpf, superAdm)
+
+        if (adm === null) {
+            console.log("Dados de ADM invalidos.")
+        } else {
+            console.log("Dados criados.");
+        }
+    } catch (e) {
+        console.log("Não deu.")
+    }
+
 }

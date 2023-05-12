6c5d770024d181d7f1e2979a26882518cbd70ba6
leticiaribeiro7 <letsribeiro2@gmail.com>
2023-05-12 19:31:22 -0300

    :construction: consumindo api na pagina de evento

--- src/app/event-details/[id]/page.tsx
+++ src/app/event-details/[id]/page.tsx
@@ -1,14 +1,28 @@
-import { MdOutlineShare, MdCalendarMonth, MdLocationOn } from "react-icons/md";
+import { 
+    MdOutlineShare,
+    MdCalendarMonth,
+    MdLocationOn 
+} from "react-icons/md";
+
 import { ImClock } from "react-icons/im";
 import Tickets from '@/components/event/Tickets';
 
 
-export default function Event() {
+
+ async function loadEvent(id: Number) {
+    const res = await fetch(`http://localhost:3000/api/evento/${id}`);
+    return res.json();
+}
+
+export default async function Event({params} : any) {
+
+   const evento = await loadEvent(params.id)
+
     return (
         <div className="mx-auto w-full h-full">
             <div className='flex justify-center lg:h-[70%] md:h-1/2 sm:h-1/2 mb-1 mt-2'>
                 <img 
-                    className='w-[95%] m-5 items-center rounded-2xl'
+                    className='w-[95%] m-5 items-center object-fit rounded-2xl'
                     src="/img/event-banner/show_djavan.jpeg"
                     alt='Imagem do evento'
                 />
@@ -18,7 +32,7 @@ export default function Event() {
                 <div>
                     <div className='flex flex-col items-center'>
                         <p className="text-2xl font-bold m-6 ">
-                            Djavan Turne A 2023
+                           {evento.nome}
                         </p>
                         <button className="shadow-xl text-gray-100 font-bold bg-deep-purple-400 flex items-center gap-3 rounded-full p-2">
                             <MdOutlineShare size={'1rem'}/>
@@ -28,29 +42,35 @@ export default function Event() {
 
                     <div className="flex flex-wrap mt-8">
                         <MdCalendarMonth color={'#6a1b9a'} size={'1.5rem'}/>
-                        <p className="font-semibold ml-2 mr-5 text-blue-gray-900">Domingo, 02/04/2023</p>
+                        <p className="font-semibold ml-2 mr-16 text-blue-gray-900">Domingo, 02/04/2023</p>
                         
                         <ImClock color={'#6a1b9a'} size={'1.5rem'}/>
                         <p className="font-semibold ml-2 mr-5 text-blue-gray-900">21:00 - 00:00</p>
+                        
                     </div>
 
-                    <div className="flex w-full mt-5">
+                    <div className="flex mt-5">
                         <MdLocationOn color={'#6a1b9a'} size={'1.5rem'}/>
                         <p className="font-semibold ml-2 mr-5 text-blue-gray-900">CLASSIC HALL - Av. Gov Agamenon Magalhães S/N, Recife - Pernambuco</p>
                     </div>
 
-                    <p className='font-bold mt-5 text-blue-gray-900'>Descrição</p>
+                    <p className='font-bold mt-16 text-blue-gray-900'>Descrição</p>
                     <div className='lg:flex md:flex mt-2 text-justify justify-between'>
                         <div className='md:w-[50%] lg:w-[50%] mb-10'>
-                            <p className='text-blue-gray-900'> O cantor Djavan, que encerrou a turnê Vesúvio no palco do Classic Hall, volta à casa de shows em Olinda, no dia 2 de abril, com sua mais nova turnê, "D". Além das canções do álbum de inéditas, ele traz um repertório recheado de grandes sucessos da sua carreira. As vendas dos ingressos terão início no dia 6 de dezembro na bilheteria do Classic Hall e online através do site Sympla. 
-                                O novo trabalho de Djavan conta com 12 faixas inéditas, incluindo a parceria com Milton Nascimento em Beleza Destruída. O disco está disponível nas plataformas digitais pela Sony Music, via Luanda Records. Djavan também reuniu sua família na faixa Iluminado, com as participações de Flávia Viana, Max Viana, João Viana, Sofia Viana, Inácio Viana, Thomas Boljover & Lui Viana.
+                            <p className='text-blue-gray-900'> 
+                            {evento.descricao}
                             </p> 
                         </div>
 
-                        <div className='lg:grid bg-gray-200 rounded-lg'>
-                            <Tickets/>
-                            <Tickets/>
-                            <Tickets/>
+                        <div className='shadow-2xl lg:w-[30%] lg:flex-col text-center items-center gap-20 inline-block bg-gray-300 rounded-lg p-3'>
+                            <Tickets setor="Área Vip"/>
+                            <Tickets setor="Camarote"/>
+                            <Tickets setor="Backstage"/>
+
+                            <p className="mb-5">Subtotal: R$0,00</p>
+                            <button type="submit" className="m-auto shadow-xl text-gray-100 font-bold bg-deep-purple-400 flex gap-3 rounded-full p-2">
+                                Adicionar ao carrinho
+                            </button>           
                         </div>
 
                     </div>
@@ -61,4 +81,3 @@ export default function Event() {
         </div>
     )
 }
-

47b1e60878726cf2d69aa0fd6c9d86cffb75ae13
Vanderleicio <68967481+Vanderleicio@users.noreply.github.com>
2023-05-12 21:04:10 -0300

    Finalização da página inicial

--- src/components/home/carrossel/index.tsx
+++ src/components/home/carrossel/index.tsx
@@ -7,22 +7,22 @@ export default function Carrossel(props: any){
         <img
           src= {props.imagem1}
           alt="image 1"
-          className="h-full w-full object-cover"
+          className="h-full w-full object-cover lg:px-[150px]"
         />
         <img
           src={props.imagem2}
           alt="image 2"
-          className="h-full w-full object-cover"
+          className="h-full w-full object-cover lg:px-[150px]"
         />
         <img
           src={props.imagem3}
           alt="image 3"
-          className="h-full w-full object-cover"
+          className="h-full w-full object-cover lg:px-[150px]"
         />
         <img
           src={props.imagem3}
           alt="image 4"
-          className="h-full w-full object-cover"
+          className="h-full w-full object-cover lg:px-[150px]"
         />
       </Carousel>
     )

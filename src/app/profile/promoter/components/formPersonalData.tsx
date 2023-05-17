dd47edeb35215ae9396fc170f027ca5a45d9beff
Washington Oliveira <wdeoliveiraj@gmail.com>
2023-05-17 17:51:19 -0300

    style: profile promoter screen adjustment

--- src/app/profile/promoter/components/formPersonalData.tsx
+++ src/app/profile/promoter/components/formPersonalData.tsx
@@ -35,7 +35,7 @@ export default function FormPersonalData() {
             city: '',
             state: '',
             cep: '',
-			selectField: 'cpf'
+			      selectField: 'cpf'
         },
 
     });

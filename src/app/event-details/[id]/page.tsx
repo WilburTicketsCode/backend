d0840a76f254b0150a57187433a3a2444e6cac49
leticiaribeiro7 <letsribeiro2@gmail.com>
2023-05-17 17:13:46 -0300

    :construction: alterando tags html e formatando valores

--- src/app/event-details/[id]/page.tsx
+++ src/app/event-details/[id]/page.tsx
@@ -27,10 +27,10 @@ export default async function Event({params} : any) {
 
     return (
         
-        <div>
+        <div className="bg-gradient-to-br from-indigo-300 via-purple-800 to-blue-200">
             <div className='flex justify-center'>
                 <img 
-                    className='w-[95%] h-[500px] m-5 items-center object-fit rounded-2xl'
+                    className='w-[95%] lg:h-[500px] m-5 items-center max-w-full h-auto object-fit rounded-2xl'
                     src="/img/event-banner/show_djavan.jpeg"
                     alt='Imagem do evento'
                 />
@@ -38,42 +38,41 @@ export default async function Event({params} : any) {
 
             <div className='shadow-2xl mr-6 ml-6 flex-row rounded-lg bg-gray-100 p-10'>
                 <div className='flex flex-col items-center'>
-                    <p className="text-2xl font-bold m-6">
+                    <h1 className="text-2xl font-bold m-6">
                         {evento.nome}
-                    </p>
+                    </h1>
                     <Button color="purple" size="md" className="flex items-center gap-3 rounded-full">
                         <MdOutlineShare size={'1rem'}/>
                         Compartilhar
                     </Button>
                 </div>
 
-                <div className="flex flex-wrap mt-8">
-                    <MdCalendarMonth color={'#6a1b9a'} size={'1.5rem'}/>
-                    <p className="font-semibold ml-2 mr-16 text-blue-gray-900">
+                <div className="flex flex-shrink mt-8 text-sm">
+                    <div className="flex flex-wrap"><MdCalendarMonth color={'#6a1b9a'} size={'1rem'}/></div>
+                    <h3 className="font-semibold ml-2 mr-16 text-blue-gray-900">
                         {moment(evento.horaInicio).format("DD/MM/YYYY")}
-                    </p>
+                    </h3>
                     
-                    <ImClock color={'#6a1b9a'} size={'1.5rem'}/>
-                    <p className="font-semibold ml-2 mr-5 text-blue-gray-900">
+                    <div className="flex flex-wrap"><ImClock color={'#6a1b9a'} size={'1rem'}/></div>
+                    <h3 className="font-semibold ml-2 mr-5 text-blue-gray-900">
                         {moment(evento.horaInicio).format("HH:mm")} - {moment(evento.horaFim).format("HH:mm")}
-                    </p>
-                    
+                    </h3>
                 </div>
 
-                <div className="flex mt-5">
-                    <MdLocationOn color={'#6a1b9a'} size={'1.5rem'}/>
-                    <p className="font-semibold ml-2 mr-5 text-blue-gray-900">
+                <div className="flex flex-shrink mt-5 text-sm">
+                    <div className="flex flex-wrap"><MdLocationOn color={'#6a1b9a'} size={'1.5rem'}/></div>
+                    <h3 className="font-semibold ml-2 mr-5 text-blue-gray-900">
                         {`${evento.endereco.rua}, ${evento.endereco.numero}, ${evento.endereco.bairro}, ${evento.endereco.cidade} - ${evento.endereco.estado}`}
-                    </p>
+                    </h3>
                 </div>
 
-                <p className='font-bold mt-16 text-blue-gray-900'>Descrição</p>
+                <h2 className='font-bold mt-16 text-blue-gray-900'>Descrição</h2>
                 <div className='lg:flex lg:gap-10 grid mt-2 text-justify justify-between'>
 
-                    <div className='md:w-[50%] lg:w-[50%] mb-10'>
-                        <p className='text-blue-gray-900'> 
+                    <div className='lg:w-[50%] mb-10'>
+                        <h3 className='text-blue-gray-900 text-sm'> 
                             {evento.descricao}
-                        </p> 
+                        </h3> 
                     </div>
 
                     <div className='shadow-2xl text-center gap-20 grid-cols-3 bg-gray-300 rounded-lg p-3'>

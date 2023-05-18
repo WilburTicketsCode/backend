9b90f38f8e5486bce04ea83ea482df4927b2fe19
Wagner Alexandre <wagner.afjr@gmail.com>
2023-05-18 19:59:14 -0300

    :bug: fix: Tela perfil adm - Alinhamento dos elementos do perfil

--- src/app/profile/admin/page.tsx
+++ src/app/profile/admin/page.tsx
@@ -2,10 +2,9 @@ import '../../../styles/globals.css'
 import AdminP from "../../../components/Admin/profile";
 
 export default function Dash() {
-    return(
-        <div className='w-full h-[90%]'>
-
-            <AdminP/>
+    return (
+        <div className='flex-col justify-center items-center w-full h-[90%]'>
+            <AdminP />
 
         </div>
         

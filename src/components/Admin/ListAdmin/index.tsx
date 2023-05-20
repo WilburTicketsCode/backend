6ef98d3f97bc290f3eb26b09cbf0b4eda4e13979
Nathielle Alves <telly.cerq@gmail.com>
2023-05-19 23:10:34 -0300

    :construction: Antes do Merge

--- src/components/Admin/ListAdmin/index.tsx
+++ src/components/Admin/ListAdmin/index.tsx
@@ -16,7 +16,7 @@ interface ListProps {
 export default function TelaListAdm(props: ListProps) {
 
     return (
-        <section className='bg-gray-200 text-blue-900 rounded-xl p-10 m-5 w-auto h-auto flex gap-20'>
+        <section className='bg-gray-200 text-blue-900 rounded-xl p-10 m-5 w-auto h-auto flex flex-col gap-20'>
             
             <Card color="transparent" shadow={false}>
                 <Typography variant="h4" color="blue-gray">
@@ -29,8 +29,22 @@ export default function TelaListAdm(props: ListProps) {
                         <li key={admin.id}>Email: {admin.email}</li>
                         <li key={admin.id}>CPF: {admin.cpf}</li>
                         <li key={admin.id}>Senha: {admin.senha}</li>
-                        </ul>
+                    </ul>
                     ))}
+
+                    <ul className="bg-gray-300 mt-10 mr-5 flex flex-col gap-6">
+                        <li className="font-semibold">Maria Gabriela Fernandes</li>
+                        <li>Email: mgfernandes@wiltickets.org</li>
+                        <li>CPF: 030.321.538-20</li>
+                        <li>Senha: admin321</li>
+                    </ul>
+
+                    <ul className="bg-gray-300 mt-10 mr-5 flex flex-col gap-6">
+                        <li className="font-semibold">João Almeida da Silva</li>
+                        <li>Email: jadsilva@wiltickets.org</li>
+                        <li>CPF: 045.123.764-90</li>
+                        <li>Senha: novoAdmin321</li>
+                    </ul>
             </Card>
 
             <Link href="/admin">

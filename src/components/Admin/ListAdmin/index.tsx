dbc4d02c2b96ebb5b624c0ac350d1a8beb5d5d7c
Nathielle Alves <telly.cerq@gmail.com>
2023-05-26 16:16:31 -0300

    📦build: Puxando os ADMs da API

--- src/components/Admin/ListAdmin/index.tsx
+++ src/components/Admin/ListAdmin/index.tsx
@@ -1,4 +1,5 @@
 import { Card, Button, Typography, } from "@/components/ClientSide";
+import { useState, useEffect } from "react";
 import Link from 'next/link';
 
 interface Adm {
@@ -13,20 +14,11 @@ interface ListProps {
     admsBD: Adm[];
 }
 
-
 //Forma de exibir tirando os dados da API (Não vai ser utilizado agora)
-/**{props.admsBD.map((admin) => (
-    <ul className="bg-gray-300 mt-10 mr-5 flex flex-col gap-6">
-        <li key={admin.id} className="font-semibold">{admin.nome}</li>
-        <li key={admin.id}>Email: {admin.email}</li>
-        <li key={admin.id}>CPF: {admin.cpf}</li>
-        <li key={admin.id}>Senha: {admin.senha}</li>
-    </ul>
-    ))}*/
 
-export default function TelaListAdm() {
+export default function TelaListAdm(props: ListProps) {
 
-    /*Consulta a API
+    //Consulta a API
     const [admins, setAdmins] = useState<Adm[]>([]);
  
     const fetchAdmins = async () => {
@@ -37,11 +29,11 @@ export default function TelaListAdm() {
  
     useEffect(() => {
         fetchAdmins();
-    }, []);*/
+    }, []);
 
     //Fim Consulta a API
 
-    const fakeApi = [{
+    /*const fakeApi = [{
         name: "Maria Gabriela Fernandes",
         email: "mgfernandes@wiltickets.org",
         CPF: "030.321.538-20",
@@ -50,31 +42,32 @@ export default function TelaListAdm() {
         name: "João Almeida da Silva",
         email: "jadsilva@wiltickets.org",
         CPF: "045.123.764-90",
-    }]
-    return (
-        <section className='bg-gray-200 text-blue-900 rounded-xl p-10 m-5 w-auto h-auto flex flex-col gap-20'>
+    }]*/
 
-            <Card color="transparent" shadow={false}>
-                <Typography variant="h4" color="blue-gray">
-                    Administradores:
-                </Typography>
-                {
-                    fakeApi.map((admin) => (
-                        <ul className="bg-gray-300 mt-10 mr-5 flex flex-col gap-6">
-                            <li className="font-semibold">Nome: {admin.name}</li>
-                            <li>Email: {admin.email}</li>
-                            <li>CPF: {admin.CPF}</li>
-                        </ul>
-                    ))
-                    }
-            </Card>
+return (
+    <section className='bg-gray-200 text-blue-900 rounded-xl p-10 m-5 w-auto h-auto flex flex-col gap-20'>
 
-            <Link href="/administrador/admin-new">
-                <Button className="mt-20" fullWidth>
-                    Cadastrar Novo Administrador
-                </Button>
-            </Link>
+        <Card color="transparent" shadow={false}>
+            <Typography variant="h4" color="blue-gray">
+                Administradores:
+            </Typography>
 
-        </section>
-    )
+            {props.admsBD.map((admin) => (
+                <ul className="bg-gray-300 mt-10 mr-5 flex flex-col gap-6">
+                    <li key={admin.id} className="font-semibold">{admin.nome}</li>
+                    <li key={admin.id}>Email: {admin.email}</li>
+                    <li key={admin.id}>CPF: {admin.cpf}</li>
+                    <li key={admin.id}>Senha: {admin.senha}</li>
+                </ul>
+            ))}
+        </Card>
+
+        <Link href="/administrador/admin-new">
+            <Button className="mt-20" fullWidth>
+                Cadastrar Novo Administrador
+            </Button>
+        </Link>
+
+    </section>
+)
 }

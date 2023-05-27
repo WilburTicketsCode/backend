67f4020d440baedbd90ee425d4db2d250058838f
Nalbertsan <nalbert1000@gmail.com>
2023-05-27 02:58:07 -0300

    :construction: Perfil de usuário 2/3 feito

--- src/components/CostumerProfile/RegistrationData/index.tsx
+++ src/components/CostumerProfile/RegistrationData/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Typography, Avatar } from "../../ClientSide";
-import { format } from 'date-fns';
 
 
 type Props = {
@@ -17,11 +16,11 @@ export default function RegistrationData({ cType }: Props) {
             <div className="bg-white flex flex-col w-[100%] md:w-[50%] h-[30rem]  items-center justify-center gap-4 overflow-auto rounded-lg">
                 <Avatar className="cursor-pointer" src="/img/profile/placeholder.jpg" alt="avatar" size="xxl" />
                 <div className="flex flex-col gap-1">
-                    <Typography className="text-base font-semibold text-center sm:text-lg">Nome: {cType?.nome}</Typography>
-                    <Typography className="text-base font-semibold text-center sm:text-lg">CPF: {cType?.cliente?.cpf.replace(/(\d{3})(\d{3})(\d{2})(\d{2})/, "$1.***.***-$4")}</Typography>
-                    <Typography className="text-base font-semibold text-center sm:text-lg">Email: {cType?.email}</Typography>
-                    <Typography className="text-base font-semibold text-center sm:text-lg">Data de Nasc.: { cType?.cliente?.data_nasc.replace(/^(\d{4})-(\d{2})-(\d{2}).*/, '$3/$2/$1')}</Typography>
-                    <Typography className="text-base font-semibold text-center sm:text-lg">Telefone: {cType?.cliente?.telefone.replace(/^(\d{2})(\d{4,5})(\d{4})$/, '($1) $2-$3')}</Typography>
+                    <Typography className="text-base font-semibold text-center sm:text-lg">Nome: {cType?.usuario?.nome}</Typography>
+                    <Typography className="text-base font-semibold text-center sm:text-lg">CPF: {cType?.cpf.replace(/(\d{3})(\d{3})(\d{2})(\d{2})/, "$1.***.***-$4")}</Typography>
+                    <Typography className="text-base font-semibold text-center sm:text-lg">Email: {cType?.usuario?.email}</Typography>
+                    <Typography className="text-base font-semibold text-center sm:text-lg">Data de Nasc.: { cType?.data_nasc.replace(/^(\d{4})-(\d{2})-(\d{2}).*/, '$3/$2/$1')}</Typography>
+                    <Typography className="text-base font-semibold text-center sm:text-lg">Telefone: {cType?.telefone.replace(/^(\d{2})(\d{4,5})(\d{4})$/, '($1) $2-$3')}</Typography>
                 </div>
             </div>
         </div>

322db625bd333f9ad6b51972770c462ad544e8ac
Washington Oliveira <wdeoliveiraj@gmail.com>
2023-06-03 00:10:22 -0300

    Update formPersonalData.tsx

--- src/components/Promoter/formPersonalData.tsx
+++ src/components/Promoter/formPersonalData.tsx
@@ -132,9 +132,9 @@ export default function FormPersonalData(props: any) {
             </div>
 
 			{/* Endereço */}
-            <div className="col-span-full">
+            <div className="sm:col-span-2 sm:col-start-1">
               <label className="block text-sm font-medium leading-6 text-gray-900">
-                Endereço
+                Rua
               </label>
               <div className="mt-2">
                 <input
@@ -142,9 +142,62 @@ export default function FormPersonalData(props: any) {
                     type="text"
                     className="block w-full rounded-md px-3 border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
+				{errors.street?.message && <span className="text-red-500">{errors.street?.message}</span>}
               </div>
-			  {errors.street?.message && <span className="text-red-500">{errors.street?.message}</span>}
             </div>
+
+            <div className="sm:col-span-1">
+              <label className="block text-sm font-medium leading-6 text-gray-900">
+                Número
+              </label>
+              <div className="mt-2">
+                <input
+					{...register('number')}
+                    type="text"
+                    className="block w-full rounded-md px-3 border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+				{errors.number?.message && <span className="text-red-500">{errors.number?.message}</span>}
+              </div>
+            </div>
+            <div className="sm:col-span-1">
+              <label className="block text-sm font-medium leading-6 text-gray-900">
+                Bairro
+              </label>
+              <div className="mt-2">
+                <input
+					{...register('neighborhood')}
+                    type="text"
+                    className="block w-full rounded-md px-3 border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+				{errors.neighborhood?.message && <span className="text-red-500">{errors.neighborhood?.message}</span>}
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label className="block text-sm font-medium leading-6 text-gray-900">
+                Complemento
+              </label>
+              <div className="mt-2">
+                <input
+					{...register('complement')}
+                    type="text"
+                    className="block w-full rounded-md px-3 border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+				{errors.complement?.message && <span className="text-red-500">{errors.complement?.message}</span>}
+              </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
 
 			{/* Cidade */}
             <div className="sm:col-span-2 sm:col-start-1">

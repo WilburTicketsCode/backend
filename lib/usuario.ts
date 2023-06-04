193c14be456c922cd772a7b1321c01ee615ab443
Pedro Henrique A <phaalmeida1@gmail.com>
2023-06-04 16:30:29 -0300

    :recycle: refactor: Edição de senha na API usuario
    Codigo refatorado para que a edição da senha seja feita na API do usuario e não da do cliente, agora qualquer tipo de usuario com login pode alterar sua senha.

--- lib/usuario.ts
+++ lib/usuario.ts
@@ -2,6 +2,11 @@ import { Prisma } from "@prisma/client";
 import { prisma } from "./prisma";
 
 export type Usuario = Prisma.PromiseReturnType<typeof getUsuario>;
+export type edicaoUsuarioTipo = {
+  tipo: string,
+  novoDado: string,
+  emailDoUsuario: string
+}
 
 export async function getUsuarios() {
   const data = await prisma.usuario.findMany({
@@ -33,4 +38,20 @@ export async function getUsuario(email: string) {
     });
 
     return data
+}
+
+export async function edicaoUsuario(tipoDeEdicao: string, novoDadoAlterado: string, emailUsuario: string) {
+  if (tipoDeEdicao === 'trocar senha') {
+    try {
+      const user = await prisma.usuario.update({
+        where: { email: emailUsuario },
+        data: { senha: novoDadoAlterado },
+      });
+      console.log('Usuario atualizado:', user);
+      return user
+    } catch (error) {
+      console.error('Erro ao atualizar o usuário:', error);
+      return null
+    }
+  }
 }
--- src/app/api/usuario/route.ts
+++ src/app/api/usuario/route.ts
@@ -1,9 +1,23 @@
 import { NextResponse } from "next/server"
-import { getUsuarios } from "../../../../lib/usuario"
+import { edicaoUsuario, edicaoUsuarioTipo, getUsuarios } from "../../../../lib/usuario"
 
 
 export async function GET(request: Request) {
     const data = await getUsuarios()
     
     return NextResponse.json(data)
+}
+
+export async function PUT(request:Request) {
+    const dados: edicaoUsuarioTipo = await request.json()
+    if (dados !== null) {
+        const clienteAlterado = await edicaoUsuario(dados.tipo, dados.novoDado, dados.emailDoUsuario)
+        if (clienteAlterado !== null){
+            return NextResponse.json(clienteAlterado)
+        } else {   
+            console.log("DEU UM ERRO")
+            return NextResponse.json({error: "ERROR 00"})
+        }
+    }
+
 }

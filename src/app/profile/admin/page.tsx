8f7eaa1333caf9bbe318edf77c69a47a094def71
Wagner Alexandre <wagner.afjr@gmail.com>
2023-06-04 18:25:07 -0300

    :tada: feat: Admin: Footer e Pagina de Perfil

--- src/app/profile/admin/page.tsx
+++ src/app/profile/admin/page.tsx
@@ -1,13 +1,18 @@
 'use client'
 import '../../../styles/globals.css'
 import AdminP from "../../../components/Admin/profile";
+import AdminFooter from '@/components/Footer/AdminFooter';
+
+
 
 export default function Dash() {
     return (
     <div>
         <div className='flex-col justify-center items-center w-full'>
             <AdminP />
-        
+
+            <AdminFooter/>
+
         </div>
     </div>
     )

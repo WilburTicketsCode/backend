ddee8fc277ebf18115b9837ecef455ac858d0d60
Pedro Henrique A <phaalmeida1@gmail.com>
2023-06-04 22:34:46 -0300

    :recycle: refactor: verificação de login alterada para usuario.ts
    Mudando a verificação de login pois ela é necessaria na hora de mudar a senha do usuario, então é melhor que a função capaz de realizar esse veirificação fique fora da API e possa ser utilizada nas outras funções, como por exemplo alterarSenha

--- lib/usuario.ts
+++ lib/usuario.ts
@@ -8,6 +8,65 @@ export type edicaoUsuarioTipo = {
   novoDado: string,
   emailDoUsuario: string
 }
+export type alterarSenhaType = {
+  email: string,
+  senhaAntiga: string,
+  senhaNova: string,
+}
+
+export async function verificarEmailESenha(email:string, senha:string) {
+  const user = await prisma.usuario.findUnique({
+    where: {
+      email: email,
+    },
+    include: {
+      adm: true,
+      promoter: true,
+      cliente: true
+    }
+  });
+
+if (user && user.senha === senha){
+    if (user.adm !== null){
+        const userWithoutPass = {
+            name: user.nome,
+            email: user.email,
+            role: "administrador",
+            id: user.adm.cpf
+        }
+        return userWithoutPass
+    } else if (user.promoter !== null){
+        if (user.promoter && user.promoter.cnpj !== null){
+            const userWithoutPass = {
+                name: user.nome,
+                email: user.email,
+                role: "promoter",
+                id: user.promoter.cnpj
+            }
+            return userWithoutPass
+        } else if (user.promoter && user.promoter.cpf !== null){
+            const userWithoutPass = {
+                name: user.nome,
+                email: user.email,
+                role: "promoter",
+                id: user.promoter.cpf
+            }
+            return userWithoutPass
+        }
+        
+    } else if (user.cliente !== null) {
+        const userWithoutPass = {
+            name: user.nome,
+            email: user.email,
+            role: "cliente",
+            id: user.cliente.cpf
+        } 
+        return userWithoutPass
+    }
+} else {
+  throw new usuarioNaoEncontrado('Login falhou, email ou senha errados.')
+}
+}
 
 export async function getUsuario(email: string) {
   const data = await prisma.usuario.findUnique({
@@ -29,16 +88,6 @@ export async function getUsuario(email: string) {
 }
 
 export async function edicaoUsuario(tipoDeEdicao: string, novoDadoAlterado: string, emailUsuario: string) {
-  if (tipoDeEdicao === 'trocar senha') {
-    try {
-      const user = await prisma.usuario.update({
-        where: { email: emailUsuario },
-        data: { senha: novoDadoAlterado },
-      });
-      console.log('Usuario atualizado:', user);
-      return user
-    } catch (error) {
-      throw new usuarioNaoEncontrado('Erro ao atualizar o usuário.')
-    }
-  } 
+  
 }
+

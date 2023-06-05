8cc257e9354e3b5e3e33c9824fa0c1acaad46284
Pedro Henrique A <phaalmeida1@gmail.com>
2023-06-05 18:20:06 -0300

    update

--- src/app/api/cliente/route.ts
+++ src/app/api/cliente/route.ts
@@ -1,16 +1,6 @@
 import { NextResponse } from "next/server";
 import { Cliente, edicaoCliente, edicaoClienteTipo, getClientes, inserirCliente } from "../../../../lib/cliente";
 import { cpfDuplicado, emailDuplicado } from "../../../../lib/erros";
-import { mailOptions, transporter, trocarDestinatario } from "../../../../lib/nodemailer";
-
-
-/* Banco pessoal
-mysql://root:(SENHA)@localhost:3306/(NOME DO BANCO) 
-*/
-
-/* Banco PlanetScale (Com erro)
-mysql://nh7ntf3fxeucxnnj0c6s:pscale_pw_Xy42bmYPRmC8byFrTK6SzV7jv4OABObnuFmuSZogCrY@aws.connect.psdb.cloud/bd_wilticket?sslaccept=strict
-*/
 
 export async function GET(request: Request) {
     const data = await getClientes()
@@ -27,15 +17,6 @@ export async function POST(request:Request) {
                 return NextResponse.json("ERROR 00")
             } else {
                 console.log("Dados criados.\n", cliente);
-                trocarDestinatario(dados.usuario.email)
-                    await transporter.sendMail({
-                        ...mailOptions,
-                        subject: 'Verificando Conta Wilbor',
-                        text: 'Email vindo diretamente do mado do backend',
-                        html: '<h1>MAGO DO BACKEND</h1><p>Email enviado pelo mago do backend' +
-                        ' quando sua conta foi criada no melhor site do universo. Sinta-se' +
-                        ' honrado de estar recebendo o email do mago do beck-end Pedro VI</p>'
-                    })
                 return NextResponse.json(cliente)
             }
 

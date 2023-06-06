71dce8ff6787b64ebc3edab9da0553d2073574e4
Washington Oliveira <wdeoliveiraj@gmail.com>
2023-06-06 17:51:14 -0300

    feat: add lifecycle in profile/promoter

--- src/app/profile/promoter/page.tsx
+++ src/app/profile/promoter/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-
 import FormSecurityData from "../../../components/Promoter/formSecurityData";
 import FormPersonalData from "../../../components/Promoter/formPersonalData";
 import {useState,useEffect} from "react";
@@ -14,22 +13,23 @@ import {
   } from "../../../components/ClientSide";
 import { Spinner } from "@material-tailwind/react";
 
+
+enum lifepoint {
+    LOADING,
+    ERROR,
+    READY,
+}
+
+
+
 export default function PromoterPage() {
-    const obj = {
-        name: 'João',
-        cpf_cnpj: '00000000000',
-        email: 'joazinho@uefs.br',
-        phone: '11900000000',
-        addres: 'Avenida Uefs',
-        city: 'Feira de Santana City',
-        state: 'Bahia',
-        cep: '000000-00',
-        selectField: 'cpf'
-    };
     // Para controle do Tabs
     const [type, setType] = useState("personal");
     // Objeto com dados do promoter a serem passados
     const [dataPromoter, setDataPromoter] = useState<any | null>(null);
+    // Controlando o status do ciclo de vida do componente
+    const [statusComponent, setStatusComponent] = useState<lifepoint>(lifepoint.LOADING);
+
 
     const {data: session} = useSession();
     const cpfORcnpj = session?.user?.id;
@@ -42,10 +42,9 @@ export default function PromoterPage() {
                     const response = await fetch(`/api/promoter/${cpfORcnpj}`);
                     const jsonData = await response.json();
                     setDataPromoter(jsonData);
-                    console.log(jsonData)////////
+                    setStatusComponent(lifepoint.READY)
                   } catch (error) {
-                    // console.error('Error fetching data:', error);
-                    console.log('Errei')
+                    setStatusComponent(lifepoint.ERROR)
                   }
             }
         };
@@ -54,8 +53,8 @@ export default function PromoterPage() {
 
     return(
         <>
-            {/** Só carrega se já tiver os dados */}
-            {dataPromoter &&  
+            {/** Se o componente estiver com todos os dados para exibição */}
+            {statusComponent == lifepoint.READY &&  
                 <div className='w-full h-full flex items-center justify-center'>  
                     <div  className='box-gray w-full h-[90%] md:h-[80%] md:w-[50%] gap-6 flex flex-col justify-center items-center overflow-auto bg-white rounded-xl mt-20 md:mt-36  mb-20'>
                         <Tabs value={type} >
@@ -84,12 +83,22 @@ export default function PromoterPage() {
                 </div>
             }
 
-            {/**Se não tiver, exibe dialog */}
-            {!dataPromoter && 
+            {/** Se ainda estiver aguardando a resposta da api */}
+            {statusComponent == lifepoint.LOADING && 
                 <div className='w-[100vw] h-[100vh] flex items-center justify-center background-load'>
                     <div className="box-gray min-w-[300px] min-h-[300px] flex flex-col items-center justify-center">
                         <b><p className="mb-20 mx-auto">Carregando</p></b>
                         <Spinner className="h-24 w-24" />
+                    </div>
+                </div>   
+            }
+
+            {/** Se houver algum erro na api */}
+            {statusComponent == lifepoint.ERROR && 
+                <div className='w-[100vw] h-[100vh] flex items-center justify-center background-load'>
+                    <div className="box-gray min-w-[300px] min-h-[300px] flex flex-col items-center justify-center">
+                        <b><p className="mb-10 mx-auto">ERRO</p></b>
+                        <p>Tivemos problemas, tente novamente mais tarde</p>
                     </div>
                 </div>   
             }

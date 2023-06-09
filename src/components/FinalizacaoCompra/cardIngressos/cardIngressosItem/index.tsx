660f6246b66ea7210143f88cc09216e325ce85b8
Antonio Nicassio <antonio.nicassio007@hotmail.com>
2023-06-09 00:19:37 -0300

    🚧 Atualizando tela de finalização de compra
    Agora o Card dos ingressos obtém os ingressos do carrinho de compras

--- src/components/FinalizacaoCompra/cardIngressos/cardIngressosItem/index.tsx
+++ src/components/FinalizacaoCompra/cardIngressos/cardIngressosItem/index.tsx
@@ -1,6 +1,41 @@
+"use client";
+import { Lotacao } from "@prisma/client";
+import { useState } from "react";
+import { useShoppingCart } from "@/contexts/ShoppingCartContext";
+
+
+function lotacoesDoSetor(listaLotacoes: any, idDoSetor: number) {
+    return listaLotacoes.filter((lotacao: any) => {
+        return lotacao.id_setor === idDoSetor
+    })
+}
 
 
 export default function CardIngressoItem(props: any) {
+    const { cartItems } = useShoppingCart()
+
+    let setores: number[] = []
+    setores = props.lotacoes?.reduce((resultado: number[], lot: Lotacao) => {
+        if (!resultado?.includes(lot.id_setor)) {
+            resultado?.push(lot.id_setor);
+        }
+        return resultado;
+    }, [])
+
+    const valorTotal = setores?.reduce((total, id) => {
+        const lotacoes = lotacoesDoSetor(props.lotacoes, id);
+        return total + lotacoes.reduce((valor: any, lot: any) => {
+            return valor + ((cartItems.find((it) => it.id === lot.id)?.quantidade || 0) * lot.valorTotal)
+        }, 0)
+    }, 0) 
+
+    const quantidadeIngressos = setores?.reduce((total, id) => {
+        const lotacoes = lotacoesDoSetor(props.lotacoes, id);
+        return total + lotacoes.reduce((valor: any, lot: any) => {
+            return valor + ((cartItems.find((it) => it.id === lot.id)?.quantidade || 0) )
+        }, 0)
+    }, 0) 
+
     return (
 
         <div className="card flex flex-row px-2 py-2">
@@ -13,17 +48,17 @@ export default function CardIngressoItem(props: any) {
                 <div className="top flex flex-row justify justify-between  space-x-16">
                     <div className=" flex flex-col w-16">
                         <p className=" text-[11px] font-sans pl-2">
-                            {props.nome}
+                            {props.event?.nome}
                         </p>
                     </div>
                     <div className="flex flex-col w-16">
                         <p className="text-[11px] font-sans ">
-                            Quantidade:
+                            Quantidade: 
                         </p>
                         <p className="text-[11px] font-sans  ">
-                            {props.quantidade}X
+                            {quantidadeIngressos}X
                         </p>
-                        <p className="text-[8px] font-sans my-2">Total:R${props.precoToTal}</p>
+                        <p className="text-[8px] font-sans my-2">Total:{(valorTotal)?.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</p>
                     </div>
 
 

57c470f739da2d204e95f5e27b256cb507e36c71
Washington Oliveira <wdeoliveiraj@gmail.com>
2023-06-12 22:05:33 -0300

    Update formPersonalData.tsx

--- src/components/Promoter/formPersonalData.tsx
+++ src/components/Promoter/formPersonalData.tsx
@@ -136,7 +136,6 @@ export default function FormPersonalData(props: any) {
 								<option value="cnpj">CNPJ</option>
 							</select>
 								<input
-					maxLength={props.user.cpf ? 11 : 14}
 									{...register('cpf_cnpj')}
 									type="text"
 									className="block w-full rounded-md px-3 border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"

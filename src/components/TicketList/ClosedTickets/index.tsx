73b60b71cc59171480ef6237da1f818d38f1bb27
Gabriel Sena <113054109+Gabriel-SBS@users.noreply.github.com>
2023-06-15 04:21:46 -0300

    ✨ Aplicando API na tela de Ticket do cliente
    - Consumindo a API e filtrando para mostrar os ativos e os encerrados
    - Ainda não testado pois não consegui acessar o banco de dados

--- src/components/TicketList/ClosedTickets/index.tsx
+++ src/components/TicketList/ClosedTickets/index.tsx
@@ -2,82 +2,68 @@
 import React, { useEffect, useState } from "react";
 import CardEventoAdm from "@/components/TicketList/Card";
 
-
 export default function ClosedTickets() {
+  const [numOfElement, setNumOfElement] = useState(6);
+  const [events, setEvents] = useState<any[]>([]);
 
-    
-    const [numOfElement, setNumOfElement] = useState(6);
-
-    //------ Consulta a API
-    /*
-    const [events, setEvents] = useState([]);
-    
-    const fetchEvents = async () => {
-        const reponse = await fetch("http://localhost:3000/api/evento");
-        const  data = await reponse.json();
-        setEvents(data);
+  const fetchEvents = async () => {
+    try {
+      const response = await fetch("http://localhost:3000/api/evento");
+      const data = await response.json();
+      if (Array.isArray(data)) {
+        const currentDate = new Date(); // Obter a data atual
+        const closedEvents = data.filter((event) => {
+          const endDate = new Date(event.horaFim);
+          return endDate < currentDate;
+        });
+        setEvents(closedEvents);
+      }
+    } catch (error) {
+      console.error("Error fetching events:", error);
     }
+  };
 
-    useEffect(() => {
-        fetchEvents();
-    }, []);
-    
-    */
-    // ----- Fim Consulta a API
+  useEffect(() => {
+    fetchEvents();
+  }, []);
 
-    // ----- Dados sem BD -----------
-    const data = {
-        eventData: [{
-            id: 1,
-            banner: "https://mundoconectado.com.br/uploads/chamadas/capa_145.jpg",
-            nome: "Evento 1",
-            horaInicio: "2023-06-01T21:00:00.000Z",
-            endereco: {
-                rua: "Rua da Paz",
-                bairro: "Jardim das Flores",
-                cidade: "São Paulo",
-            }
-        },
-        {
-            id: 2,
-            banner: "https://alphaconvites.com.br/wp-content/uploads/2023/01/festa-antes-formatura-scaled-1-2048x1367.jpg",
-            nome: "Evento 2",
-            horaInicio: "2023-06-01T21:00:00.000Z",
-            endereco: {
-                rua: "Rua da Paz",
-                bairro: "Jardim das Flores",
-                cidade: "São Paulo",
-            }
-        },
-        ]
-    };
-    // ----- Fim Dados sem BD -----------
+  const slice = events.slice(0, numOfElement);
 
-    const slice = data.eventData.slice(0, numOfElement);  //const slice = events.slice(0, numOfElement);
+  function formatDate(date: string): string {
+    const dd = date.slice(0, 16).replaceAll("-", "/").replaceAll("T", "-").split("-");
+    return dd[0] + " - " + dd[1];
+  }
 
-    function formatDate(date: string): string {
-        const dd = date.slice(0,16).replaceAll('-', '/').replaceAll('T', '-').split('-');   
-        return dd[0] + " - " + dd[1]
-    }
+  const loadMore = () => {
+    setNumOfElement(numOfElement + numOfElement);
+  };
 
-    const loadMore = () => {
-        setNumOfElement(numOfElement + numOfElement)
-    }
+  const areMoreEventsAvailable = numOfElement < events.length;
+  const isButtonDisabled = !areMoreEventsAvailable;
 
-    return (
-        <div className="flex flex-col h-full items-center justify-center mt-4">
-            <div className="grid grid-cols-12 md:col-span-6 xl:col-span-4 h-full xl:gap-x-14 md:gap-8 gap-y-8 mt-4 mb-4">  {/*Layout para essa página*/}
-                {slice.map((item, index) => {
-                    return (<CardEventoAdm imagemEvento={item.banner} nomeEvento={item.nome} dataEvento={formatDate(item.horaInicio)} localEvento={item.endereco.rua + ", " + item.endereco.bairro + ", " + item.endereco.cidade} evento="/ticket" />)
-                })}
-                <div className="object-center text-center col-span-12 mt-10 mb-3">
-                    <button className="bg-roxo-wil h-[36px] w-[230px] text-white font-sans text-sm font-semibold text-center object-center rounded-full shadow-md shadow-black/40" onClick={() => loadMore()}>
-                        MAIS EVENTOS
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className="flex flex-col h-full items-center justify-center mt-4">
+      <div className="grid grid-cols-12 md:col-span-6 xl:col-span-4 h-full xl:gap-x-14 md:gap-8 gap-y-8 mt-4 mb-4">
+        {slice.map((item, index) => (
+          <CardEventoAdm
+            key={index}
+            imagemEvento={item.banner}
+            nomeEvento={item.nome}
+            dataEvento={formatDate(item.horaInicio)}
+            localEvento={`${item.endereco.rua}, ${item.endereco.bairro}, ${item.endereco.cidade}`}
+            evento="/ticket"
+          />
+        ))}
+        <div className="object-center text-center col-span-12 mt-10 mb-3">
+          <button
+            className="bg-roxo-wil h-[36px] w-[230px] text-white font-sans text-sm font-semibold text-center object-center rounded-full shadow-md shadow-black/40"
+            onClick={loadMore}
+            disabled={isButtonDisabled}
+            >
+            MAIS EVENTOS
+          </button>
         </div>
-
-
-    )
+      </div>
+    </div>
+  );
 }

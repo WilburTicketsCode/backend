70edf9699ab788f4f372432d2e7b5caf19b1635b
Nathielle Alves <telly.cerq@gmail.com>
2023-06-15 17:34:36 -0300

    :construction: Detalhes finais cadastro de ADM

--- src/components/Admin/NewAdmin/index.tsx
+++ src/components/Admin/NewAdmin/index.tsx
@@ -1,10 +1,12 @@
 'use client'
-import { Input, Card, Button, Typography,} from "@/components/ClientSide";
+import { Input, Card, Button, Typography, } from "@/components/ClientSide";
 import Link from 'next/link';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { CheckCircleIcon } from "@heroicons/react/24/solid";
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import React, { useState } from 'react';
+import { Alert } from "@material-tailwind/react";
 
 type adminFormData = z.infer<typeof adminNSchema>;
 
@@ -18,9 +20,18 @@ const adminNSchema = z.object({
 }).refine((data) => data.password === data.passwordConfirm, {
     message: "Senhas diferentes",
     path: ["passwordConfirm"],
-  });
+});
 
 export default function TelaNewAdm() {
+
+    const [openAlert, setOpenAlert] = useState(false);
+
+    const buttonAction = () => {
+        setOpenAlert(true);
+        setTimeout(() => {
+            setOpenAlert(false);
+        }, 3000);
+    }
 
     const { register, handleSubmit, formState: { errors } } = useForm<adminFormData>({
         resolver: zodResolver(adminNSchema),
@@ -49,30 +60,30 @@ export default function TelaNewAdm() {
 
     const onSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-    
+
         // Enviar dados do formulário para a API
         fetch('https://backend-wilbortick.vercel.app/api/administrador', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(adminNSchema),
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(adminNSchema),
         })
-          .then((response) => {
-            if (response.ok) {
-              console.log('Dados salvos com sucesso!');
-            } else {
-              console.error('Erro ao salvar os dados!');
-            }
-          })
-          .catch((error) => {
-            console.error('Erro ao enviar os dados:', error);
-          });
+            .then((response) => {
+                if (response.ok) {
+                    console.log('Dados salvos com sucesso!');
+                } else {
+                    console.error('Erro ao salvar os dados!');
+                }
+            })
+            .catch((error) => {
+                console.error('Erro ao enviar os dados:', error);
+            });
     }
 
     return (
         <section className='bg-gray-200 flex flex-col items-center justify-center gap-y-10 p-20 rounded-xl w-auto h-auto'>
-            
+
             <Card color="transparent" shadow={false} className="flex items-center justify-center">
                 <Typography variant="h4" color="blue-gray">
                     Cadastrar Novo Administrador
@@ -84,36 +95,47 @@ export default function TelaNewAdm() {
                 {/**Formulário começa aqui */}
                 <form onSubmit={onSubmit} className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
                     <div className="min-w-fit mb-4 flex flex-col gap-6">
-                        <Input {...register('name')}  size='md'  label="Nome Completo" value={user.name}/>
+                        <Input {...register('name')} size='md' label="Nome Completo" value={user.name} />
                         {errors.name?.message && <p className="text-red-500 text-xs">{errors.name?.message}</p>}
 
                         <div className="flex flex-col gap-6 md:flex-row">
-                            <Input {...register('email')}  size='md'  label="Email" value={user.email}/>
+                            <Input {...register('email')} size='md' label="Email" value={user.email} />
                             {errors.email?.message && <p className="text-red-500 text-xs">{errors.email?.message}</p>}
 
-                            <Input {...register('cpf')}  size='md' label="CPF" value={user.cpf}/>
+                            <Input {...register('cpf')} size='md' label="CPF" value={user.cpf} />
                             {errors.cpf?.message && <p className="text-red-500 text-xs">{errors.cpf?.message}</p>}
                         </div>
 
                         <div className="flex flex-col gap-6 md:flex-row">
-                            <Input {...register('password')}  type="password" size='md' label="Senha" value={user.password}/>
+                            <Input {...register('password')} type="password" size='md' label="Senha" value={user.password} />
                             {errors.password?.message && <p className="text-red-500 text-xs">{errors.password?.message}</p>}
 
-                            <Input {...register('passwordConfirm')}  type="password" size='md' label="Confirme a senha" value={user.passwordConfirm}/>
+                            <Input {...register('passwordConfirm')} type="password" size='md' label="Confirme a senha" value={user.passwordConfirm} />
                             {errors.passwordConfirm?.message && <p className="text-red-500 text-xs">{errors.passwordConfirm?.message}</p>}
                         </div>
-                        <Button onClick={()=>{cleanInfoAdm(); handleSubmit}} type='submit' size='md' className="mt-20" fullWidth>
+                        <Button onClick={() => {cleanInfoAdm(); handleSubmit; buttonAction}} type='submit' size='md' className="mt-20" fullWidth>
                             Cadastrar
                         </Button>
                     </div>
 
                     <Link href="/administrador/admin-list">
                         <p className="flex items-center justify-center">
-                            Ver todos os administradores  
+                            Ver todos os administradores
                         </p>
                     </Link>
                 </form>
             </Card>
+
+            <Alert
+                open={openAlert}
+                color="green"
+                className="max-w-fit absolute bottom-5"
+                icon={<CheckCircleIcon className="mt-px h-6 w-6" />}
+            >
+                <p className="font-normal">
+                    Administrador Cadastrado
+                </p>
+            </Alert>
 
         </section>
     )

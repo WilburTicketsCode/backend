572aa6a8781345eb89358bf74b9a9f620839dcda
Nathielle Alves <telly.cerq@gmail.com>
2023-06-15 17:51:20 -0300

    Update index.tsx

--- src/components/Admin/NewAdmin/index.tsx
+++ src/components/Admin/NewAdmin/index.tsx
@@ -1,12 +1,9 @@
 'use client'
-import { Input, Card, Button, Typography, } from "@/components/ClientSide";
+import { Input, Card, Button, Typography,} from "@/components/ClientSide";
 import Link from 'next/link';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { CheckCircleIcon } from "@heroicons/react/24/solid";
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
-import React, { useState } from 'react';
-import { Alert } from "@material-tailwind/react";
 
 type adminFormData = z.infer<typeof adminNSchema>;
 
@@ -16,22 +13,12 @@ const adminNSchema = z.object({
     cpf: z.string().min(8, { message: 'CPF deve conter no mínimo 8 caracteres' }),
     password: z.string().min(8, { message: 'Senha deve conter no mínimo 8 caracteres' }),
     passwordConfirm: z.string().min(8, { message: 'Senha deve conter no mínimo 8 caracteres' }),
-
 }).refine((data) => data.password === data.passwordConfirm, {
     message: "Senhas diferentes",
     path: ["passwordConfirm"],
-});
+  });
 
 export default function TelaNewAdm() {
-
-    const [openAlert, setOpenAlert] = useState(false);
-
-    const buttonAction = () => {
-        setOpenAlert(true);
-        setTimeout(() => {
-            setOpenAlert(false);
-        }, 3000);
-    }
 
     const { register, handleSubmit, formState: { errors } } = useForm<adminFormData>({
         resolver: zodResolver(adminNSchema),
@@ -40,33 +27,42 @@ export default function TelaNewAdm() {
 
     });
 
-
-    const onSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-
+    const onSubmit = (data: adminFormData) => {
+        
+        const admininistrador = {
+            cpf: data.cpf,
+            super_adm: false,
+            usuario: {
+                nome: data.name,
+                email: data.email,
+                senha: data.password
+            }
+        }
+    
         // Enviar dados do formulário para a API
         fetch('/api/administrador', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(adminNSchema),
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(admininistrador),
         })
-            .then((response) => {
-                if (response.ok) {
-                    console.log('Dados salvos com sucesso!');
-                } else {
-                    console.error('Erro ao salvar os dados!');
-                }
-            })
-            .catch((error) => {
-                console.error('Erro ao enviar os dados:', error);
-            });
+          .then((response) => {
+            if (response.ok) {
+              console.log('Dados salvos com sucesso!');
+              alert('usuário cadastrado')
+            } else {
+              console.error('Erro ao salvar os dados!');
+            }
+          })
+          .catch((error) => {
+            console.error('Erro ao enviar os dados:', error);
+          });
     }
 
     return (
-        <section className='bg-gray-200 flex flex-col items-center justify-center gap-y-10 p-20 rounded-xl w-auto h-auto'>
-
+        <section className='flex flex-col w-[90%] md:w-[40%] h-[80%] md:h-[80%] items-center justify-center gap-y-10 bg-white p-20 rounded-xl'>
+            
             <Card color="transparent" shadow={false} className="flex items-center justify-center">
                 <Typography variant="h4" color="blue-gray">
                     Cadastrar Novo Administrador
@@ -76,49 +72,38 @@ export default function TelaNewAdm() {
                 </Typography>
 
                 {/**Formulário começa aqui */}
-                <form onSubmit={onSubmit} className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+                <form onSubmit={handleSubmit(onSubmit)} className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
                     <div className="min-w-fit mb-4 flex flex-col gap-6">
-                        <Input {...register('name')} size='md' label="Nome Completo" />
-                        {errors.name?.message && <p className="text-red-500 text-xs">{errors.name?.message}</p>}
+                        <Input {...register('name')}  size='md'  label="Nome Completo" />
+                        {errors.name?.message && <p className="text-red-500 text-center">{errors.name?.message}</p>}
 
                         <div className="flex flex-col gap-6 md:flex-row">
-                            <Input {...register('email')} size='md' label="Email" />
-                            {errors.email?.message && <p className="text-red-500 text-xs">{errors.email?.message}</p>}
+                            <Input {...register('email')}  size='md'  label="Email" />
+                            {errors.email?.message && <p className="text-red-500 text-center">{errors.email?.message}</p>}
 
-                            <Input {...register('cpf')} size='md' label="CPF"/>
-                            {errors.cpf?.message && <p className="text-red-500 text-xs">{errors.cpf?.message}</p>}
+                            <Input {...register('cpf')}  size='md' label="CPF" />
+                            {errors.cpf?.message && <p className="text-red-500 text-center">{errors.cpf?.message}</p>}
                         </div>
 
                         <div className="flex flex-col gap-6 md:flex-row">
-                            <Input {...register('password')} type="password" size='md' label="Senha" />
+                            <Input {...register('password')}  type="password" size='md' label="Senha" />
                             {errors.password?.message && <p className="text-red-500 text-xs">{errors.password?.message}</p>}
 
-                            <Input {...register('passwordConfirm')} type="password" size='md' label="Confirme a senha" />
+                            <Input {...register('passwordConfirm')}  type="password" size='md' label="Confirme a senha" />
                             {errors.passwordConfirm?.message && <p className="text-red-500 text-xs">{errors.passwordConfirm?.message}</p>}
                         </div>
-                        <Button onClick={() => {buttonAction}} type='submit' size='md' className="mt-20" fullWidth>
+                        <Button type='submit' size='md' className="mt-20" fullWidth>
                             Cadastrar
                         </Button>
                     </div>
 
                     <Link href="/administrador/admin-list">
                         <p className="flex items-center justify-center">
-                            Ver todos os administradores
+                            Ver todos os administradores  
                         </p>
                     </Link>
                 </form>
             </Card>
-
-            <Alert
-                open={openAlert}
-                color="green"
-                className="max-w-fit absolute bottom-5"
-                icon={<CheckCircleIcon className="mt-px h-6 w-6" />}
-            >
-                <p className="font-normal">
-                    Administrador Cadastrado
-                </p>
-            </Alert>
 
         </section>
     )

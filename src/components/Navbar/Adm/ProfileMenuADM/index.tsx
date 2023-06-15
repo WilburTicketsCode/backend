a30e42bf7370e2f08a60fdd7922ae04f18cf1056
Vanderleicio <68967481+Vanderleicio@users.noreply.github.com>
2023-06-14 22:16:51 -0300

    Conserto da navbar do ADM

--- src/components/Navbar/Adm/ProfileMenuADM/index.tsx
+++ src/components/Navbar/Adm/ProfileMenuADM/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from "react";
-
+import { signOut, useSession } from "next-auth/react";
 import {
     Typography,
     Button,
@@ -17,25 +17,18 @@ import {
     PowerIcon,
     HomeIcon
 } from "@heroicons/react/24/outline";
-
+import Link from "next/link";
 
 const profileMenuItems = [
     {
         label: "Pagina Inicial",
         icon: HomeIcon,
+        href: "/administrador/eventos"
     },
     {
         label: "Meu Perfil",
         icon: UserCircleIcon,
         href: "/profile/admin"
-    },
-    {
-        label: "Dashboard",
-        icon: Cog6ToothIcon,
-    },
-    {
-        label: "Sair",
-        icon: PowerIcon,
     },
 ];
 
@@ -65,33 +58,46 @@ export default function ProfileMenuADM() {
                 </Button>
             </MenuHandler>
             <MenuList className="p-1">
-                {profileMenuItems.map(({ label, icon }, key) => {
-                    const isLastItem = key === profileMenuItems.length - 1;
-                    return (
-                        <MenuItem
-                            key={label}
-                            onClick={closeMenu}
-                            className={`flex items-center gap-2 rounded ${isLastItem
-                                ? "hover:bg-red-500/10 focus:bg-red-500/10 active:bg-red-500/10"
-                                : ""
-                                }`}
-                        >
-                            {React.createElement(icon, {
-                                className: `h-6 w-6 ${isLastItem ? "text-red-500" : ""}`,
-                                strokeWidth: 2,
-                            })}
-                            <Typography
-                                as="span"
-                                variant="h5"
-                                className="font-normal"
-                                color={isLastItem ? "red" : "inherit"}
-                            >
-                                {label}
-                            </Typography>
-                        </MenuItem>
-                    );
-                })}
-            </MenuList>
+          {profileMenuItems.map(({ label, icon, href }, key) => {
+            return (
+              <MenuItem
+                key={label}
+                onClick={closeMenu}
+                className={`flex items-center gap-2 rounded`}
+              >
+                <Link href={href} className='flex flex-row gap-2'>
+                  {React.createElement(icon, {
+                    className: `h-6 w-6`,
+                    strokeWidth: 2,
+                  })}
+                  <Typography
+                    as="span"
+                    variant="h5"
+                    className="font-normal"
+                    color={"inherit"}
+                  >
+                    {label}
+                  </Typography>
+                </Link>
+              </MenuItem>
+            );
+          })}
+
+          <MenuItem onClick={() => signOut({ callbackUrl: '/' })} className='border-transparent p-2.5 cursor-pointer w-full bg-white flex gap-2 rounded hover:bg-red-500'>
+            {React.createElement(PowerIcon, {
+              className: `h-6 w-6 text-red-500`,
+            })}
+            <Typography
+              as="span"
+              variant="h5"
+              className="font-normal"
+              color="red"
+            >
+              Sair
+            </Typography>
+          </MenuItem>
+
+        </MenuList>
         </Menu>
     );
 }
